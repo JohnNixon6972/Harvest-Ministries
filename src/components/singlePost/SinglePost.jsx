7b0b35f2ab8bafb8d1611435860db7315c39de42
JohnNixon6972 <johnbrightnixon@gmail.com>
2022-08-30 20:16:34 +0530

    fixes

--- src/components/singlePost/SinglePost.jsx
+++ src/components/singlePost/SinglePost.jsx
@@ -16,6 +16,7 @@ export default function SinglePost() {
 
   useEffect(() => {
     const getPost = async () => {
+      console.log("/posts/" + path);
       const res = await axios.get("/posts/" + path);
       setPost(res.data);
       setTitle(res.data.title);
