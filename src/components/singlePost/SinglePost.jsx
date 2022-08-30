6d572def3d179fae8d0cdb0c64e9663a481edae9
JohnNixon6972 <johnbrightnixon@gmail.com>
2022-08-30 20:20:45 +0530

    fixing

--- src/components/singlePost/SinglePost.jsx
+++ src/components/singlePost/SinglePost.jsx
@@ -1,4 +1,4 @@
-import axios from "axios";
+import axios from "../../axios";
 import { useContext, useEffect, useState } from "react";
 import { useLocation } from "react-router";
 import { Link } from "react-router-dom";
