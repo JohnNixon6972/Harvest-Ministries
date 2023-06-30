7ed667fcf51b3ff17931127030ebd8a3b199d634
John Nixon <johnbrightnixon@gmail.com>
2023-06-30 18:00:04 +0530

    few fizes

--- src/components/sidebar/Sidebar.jsx
+++ src/components/sidebar/Sidebar.jsx
@@ -1,18 +1,5 @@
-import axios from "axios";
-import { useEffect, useState } from "react";
-import { Link } from "react-router-dom";
 import "./sidebar.css";
-
 export default function Sidebar() {
-  const [cats, setCats] = useState([]);
-
-  useEffect(() => {
-    const getCats = async () => {
-      const res = await axios.get("/categories");
-      setCats(res.data);
-    };
-    getCats();
-  }, []);
   return (
     <div className="sidebar">
       <div className="sidebarItem">
--- src/pages/settings/Settings.jsx
+++ src/pages/settings/Settings.jsx
@@ -1,5 +1,4 @@
 import "./settings.css";
-import Sidebar from "../../components/sidebar/Sidebar";
 import { useContext, useState } from "react";
 import { Context } from "../../context/Context";
 import axios from "../../axios";
