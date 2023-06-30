f4c3955675f3e52d2397e563c3a6e4f1b4ea34f4
John Nixon <johnbrightnixon@gmail.com>
2023-06-30 17:56:38 +0530

    backend connected

--- src/axios.js
+++ src/axios.js
@@ -2,6 +2,6 @@ import axios from "axios"
 
 const instance = axios.create({
     // baseURL : "https://harvest-ministries-backend.herokuapp.com/api/"
-    baseURL:"http://localhost:9000/api"
+    baseURL:"https://harvestbackend.onrender.com/api"
 });
 export default instance;
